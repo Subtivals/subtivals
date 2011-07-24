46925dd26fadd3b01f2366771391d582efa88bb8
Arnaud Rolly <github@gedial.com>
2011-07-24 18:31:50 +0200

    comments

--- script.h
+++ script.h
@@ -9,22 +9,53 @@
 #include "style.h"
 #include "event.h"
 
+/*
+ * Scipts from an ASS file : union of events and styles.
+ */
 class Script : public QObject
 {
     Q_OBJECT
 public:
+    /*
+     * Constructs a script from an ASS file.
+     */
     explicit Script(const QString &p_fileName, QObject *parent = 0);
+    /*
+     * Returns the name of the ASS file used to create the script.
+     */
     const QString &fileName() const;
+    /*
+     * Returns the script title.
+     */
     const QString &title() const;
+    /*
+     * Returns a style from its name.
+     */
     const Style *style(const QString &p_name) const;
+    /*
+     * Return the number of events in the script.
+     */
     int eventsCount() const;
+    /*
+     * Returns an iterator on the events of the script.
+     */
     const QListIterator<Event *> events() const;
-signals:
-public slots:
 private:
+    /*
+     * Script ASS file name.
+     */
     QString m_fileName;
+    /*
+     * Script title.
+     */
     QString m_title;
+    /*
+     * Script styles map.
+     */
     QMap<QString, Style *> m_styles;
+    /*
+     * Script events list.
+     */
     QList<Event *> m_events;
 };
 
--- style.h
+++ style.h
@@ -6,17 +6,42 @@
 #include <QFont>
 #include <QColor>
 
+/*
+ * Style to use to show an event.
+ */
 class Style : public QObject
 {
     Q_OBJECT
 public:
+    /*
+     * Constructs a style from an style line of an ASS file.
+     */
     explicit Style(const QString &p_line, QObject *p_parent = 0);
+    /*
+     * Returns the style name.
+     */
     const QString &name() const;
+    /*
+     * Returns the style font.
+     */
     const QFont &font() const;
+    /*
+     * Returns the style primary colou.
+     * Others colours from the ASS file are ignored.
+     */
     const QColor &primaryColour() const;
 private:
+    /*
+     * Style name.
+     */
     QString m_name;
+    /*
+     * Style font.
+     */
     QFont m_font;
+    /*
+     * Style primary colour.
+     */
     QColor m_primaryColour;
 };
 
