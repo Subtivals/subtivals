85036ba7db1205aae814c74121793ca40203ffe0
Mathieu Leplatre <contact@mathieu-leplatre.info>
2014-04-09 00:14:58 +0200

    Last polish

--- js/custom.js
+++ js/custom.js
@@ -287,57 +287,6 @@ function signupOverlay() {
   });
 }
 
-/* --- Modal overlay (used for login form) ------------------- */
-
-function loginOverlay() {
-  var container = document.querySelector( 'div.container' ),
-    triggerBttn = document.querySelector( '.login' ),
-    overlay = document.querySelector( 'div#login' ),
-    closeBttn = overlay.querySelector( 'button.overlay-close' );
-    transEndEventNames = {
-      'WebkitTransition': 'webkitTransitionEnd',
-      'MozTransition': 'transitionend',
-      'OTransition': 'oTransitionEnd',
-      'msTransition': 'MSTransitionEnd',
-      'transition': 'transitionend'
-    },
-    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-    support = { transitions : Modernizr.csstransitions };
-
-  function toggleOverlay() {
-    if( classie.has( overlay, 'open' ) ) {
-      classie.remove( overlay, 'open' );
-      classie.remove( container, 'overlay-open' );
-      classie.add( overlay, 'close-me' );
-      var onEndTransitionFn = function( ev ) {
-        if( support.transitions ) {
-          if( ev.propertyName !== 'visibility' ) return;
-          this.removeEventListener( transEndEventName, onEndTransitionFn );
-        }
-        classie.remove( overlay, 'close-me' );
-      };
-      if( support.transitions ) {
-        overlay.addEventListener( transEndEventName, onEndTransitionFn );
-      }
-      else {
-        onEndTransitionFn();
-      }
-    }
-    else if( !classie.has( overlay, 'close-me' ) ) {
-      classie.add( overlay, 'open' );
-      classie.add( container, 'overlay-open' );
-    }
-  }
-
-  triggerBttn.addEventListener( 'click', toggleOverlay );
-  closeBttn.addEventListener( 'click', toggleOverlay );
-
-  $('.login').click(function(e) {
-      e.preventDefault();
-  });
-}
-
-
 
 /* --- One Page Scroll ------------------- */
 
