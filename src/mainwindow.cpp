b6a3aae74b05cabd8a3fd6cd3946684846ffe0b3
Mathieu Leplatre <mathieu@mozilla.com>
2020-03-18 11:22:28 +0100

    Fix apparence of disabled widgets in dark mode

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1147,6 +1147,7 @@ void MainWindow::actionAdvancedSettings() {
 void MainWindow::actionToggleDarkMode(bool p_enabled) {
   if (p_enabled) {
     QColor darkGray(53, 53, 53);
+    QColor gray(128, 128, 128);
     QColor black(25, 25, 25);
     QColor blue(42, 130, 218);
 
@@ -1160,10 +1161,15 @@ void MainWindow::actionToggleDarkMode(bool p_enabled) {
     darkPalette.setColor(QPalette::Text, Qt::white);
     darkPalette.setColor(QPalette::Button, darkGray);
     darkPalette.setColor(QPalette::ButtonText, Qt::white);
-    darkPalette.setColor(QPalette::BrightText, Qt::red);
     darkPalette.setColor(QPalette::Link, blue);
     darkPalette.setColor(QPalette::Highlight, blue);
     darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    darkPalette.setColor(QPalette::Active, QPalette::Button, gray.darker());
+    darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, gray);
+    darkPalette.setColor(QPalette::Disabled, QPalette::WindowText, gray);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Text, gray);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Light, darkGray);
     qApp->setPalette(darkPalette);
   } else {
     qApp->setPalette(m_defaultPalette);
